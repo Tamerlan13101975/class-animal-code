4a9cd5888ac6a9e56cbe79971e11f2b289231548
Tamerlan13101975 <166745761+Tamerlan13101975@users.noreply.github.com>
2024-05-08 19:09:29 +0300

    naw action code

--- main.py
+++ main.py
@@ -2,3 +2,13 @@ class Animal():
     def __init__(self, name, age):
         self.name = name
         self.age = age
+
+    def eat(self):
+        print(f"{self.name} кушает")
+
+    def make_sound(self):
+        pass
+
+    
+
+
